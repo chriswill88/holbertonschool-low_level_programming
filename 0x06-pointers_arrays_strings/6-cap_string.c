be71bd61860e1e7d5a1911dea4ca744991bd00c6
chriswill88 <williechri79@gmail.com>
2019-07-07 03:22:34 -0400

    task 6 complete

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+* cap_string - capitalizes first letter in words
+* @a: the string passed in
+* Return: the processed string
+*/
+char *cap_string(char *a)
+{
+	char *seperator = "\n \t ,;.!?\"(){}";
+	int i, n = 0;
+
+	while (a[n] != '\0')
+	{
+		for (i = 0; seperator[i] != '\0'; i++)
+		{
+			if (seperator[i] == a[n])
+			{
+				if (a[n + 1] >= 'a' && a[n + 1] <= 'z')
+				{
+					n++;
+					*(a + n) -= 32;
+				}
+			}
+		}
+		n++;
+	}
+	return (a);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+char *cap_string(char *);
 char *string_toupper(char *a);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
