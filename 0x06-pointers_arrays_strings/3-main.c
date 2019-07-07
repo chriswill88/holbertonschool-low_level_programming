247b9eaaee3460dfbb516bdd3c419f6d1493b707
chriswill88 <williechri79@gmail.com>
2019-07-07 02:11:17 -0400

    task 3 done

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-    char s1[] = "Hell";
-    char s2[] = "Worl!";
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
+    printf("%d\n", strcmp(s1, s2));
+    printf("%d\n", strcmp(s2, s1));
+    printf("%d\n", strcmp(s1, s1));
     return (0);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
 /**
-* _strcmp - compares the length of the strings
-* @s1: a string to compare
-* @s2: a string to compare
-* Return: 15, -15, 0
+* _strcmp - compares strings
+* @s1: a string to be compared
+* @s2: a string to be compared
+* Return: *s1 - *s2
+*
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, x = 0;
-
-	while (s1[i] != '\0')
-		i++;
-
-	while (s2[x] != '\0')
-		x++;
-
-	if (i == x)
-		return (0);
-	if (i > x)
-		return (15);
-	return (-15);
+	while (*s1 != '\0')
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+			break;
+	}
+	return (*s1 - *s2);
 }
