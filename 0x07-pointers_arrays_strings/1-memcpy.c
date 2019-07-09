c0dc1992198ce675f7c3f5b838d88e2890180d88
chriswill88 <williechri79@gmail.com>
2019-07-09 15:09:17 -0400

    the task 1 fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,16 @@
+#include "holberton.h"
+/**
+* _memcpy - copies memory area
+* @dest: the destination
+* @src: the source code
+* @n: number of bytes
+* Return: a pointer to the copy
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *ptr = dest;
 
-	while ((n > 0) && *src != '\0')
+	while (n > 0)
 	{
 		*dest++ = *src++;
 		n--;
