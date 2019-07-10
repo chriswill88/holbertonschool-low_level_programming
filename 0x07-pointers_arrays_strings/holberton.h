5fd2a5bd5ad9d3cc9dee8157eb41a0419b33490f
chriswill88 <williechri79@gmail.com>
2019-07-10 10:09:25 -0400

    task 5 completed

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
