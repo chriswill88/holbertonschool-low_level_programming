cfb7fe293e78c5fb1443dfdba379051fb23135cc
chriswill88 <williechri79@gmail.com>
2019-07-11 00:39:37 -0400

    file 7 fin

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+* _strlen_recursion - finds the string length
+* @s: the string
+* Return: the sum
+*/
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+		if (s[sum] == '\0')
+		{
+			return (0);
+		}
+		sum += 1;
+		return (sum += _strlen_recursion(s + sum));
+}
+/**
+* pal - the palindrome helper
+* @s: the string
+* @i: the string length
+* @n: the index
+* Return: 0 or 1
+*/
+int pal(char *s, int i, int n)
+{
+	if (s[i] != s[n])
+		return (0);
+	if (i >= n)
+		return (1);
+	return (pal(s, i--, n++));
+}
+/**
+* is_palindrome - finds palindrome
+* @s: the string
+* Return: 1 or 0
+*/
+int is_palindrome(char *s)
+{
+	int i;
+
+	i = _strlen_recursion(s);
+	if (i == 0)
+		return (1);
+	return (pal(s, i - 1, 0));
+}
