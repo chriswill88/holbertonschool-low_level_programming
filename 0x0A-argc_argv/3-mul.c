6f90d2c9e228e63fd558e1f4b30bb039a32fa1f8
chriswill88 <williechri79@gmail.com>
2019-07-14 10:04:53 -0400

    task 3 done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
 */
 int main(int argc, char **argv)
 {
-	int i = 0, n = 0, results = 0, place = 1, place1 = 1, x = 0;
+	int i = 0, n = 0, results = 0, place = 1, x = 0, place1 = 1;
 
 	if (argc != 3)
 	{
