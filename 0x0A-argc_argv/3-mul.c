980f1450a3a154b3b967e6ac839576429d2a02b5
chriswill88 <williechri79@gmail.com>
2019-07-14 04:57:37 -0400

    task 3 done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /**
 * main - multiplies two numbers
 * @argc: the # of arguments
@@ -7,23 +8,38 @@
 */
 int main(int argc, char **argv)
 {
-	int i = 0, n, results = 0, place = 1;
+	int z = 0, y = 0, i = 0, n = 0, results = 0, place = 1, place1 = 1, x = 0;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	while (argv[1][i] != '\0')
+	i = strlen(argv[1]) - 1;
+	n = strlen(argv[2]) - 1;
+	x = n;
+	while (i >= 0)
 	{
-		for (n = 0; argv[2][n] != '\0'; n++)
+		place = 1;
+		if (argv[1][i] == '-')
 		{
-			printf("1-->%d | 2--> %d\n", (argv[1][i] - '0') , (argv[2][n] - '0'));
-			results += (argv[1][i] - '0') * (argv[2][n] - '0') * place;
+			results *= -1;
+			i--;
+			break;
 		}
-		place *= 10;
-		i++;
+		for (n = x; n >= 0; n--)
+		{
+			if (argv[2][n] == '-')
+			{
+				n--;
+				results *= -1;
+				break;
+			}
+			results += ((argv[1][i] - '0') * place1) * ((argv[2][n] - '0') * place);
+			place *= 10;
+		}
+		place1 *= 10;
+		i--;
 	}
 	printf("%d\n", results);
 	return (1);
