bf5a0b9e3a96e4019471608a7d411ac3c4092458
chriswill88 <williechri79@gmail.com>
2019-07-16 14:32:52 -0400

    0x0b added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,8 @@ char *create_array(unsigned int size, char c)
 	if (size <= 0)
 		return (NULL);
 	ptr = malloc(size);
+	if (ptr == NULL)
+		return (NULL);
 	while (i < size)
 		ptr[i++] = c;
 	return (ptr);
