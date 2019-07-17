a969bc30b63a13bd45e3a3a651a4e9ccab4a53e0
chriswill88 <williechri79@gmail.com>
2019-07-16 20:35:32 -0400

    task 2 fin

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,6 +5,10 @@ char *str_concat(char *s1, char *s2)
 {
 	int i = 0, n = 0, space = 0, count = 0;
 	char *ptr;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[i] != '\0')
 		i++;
