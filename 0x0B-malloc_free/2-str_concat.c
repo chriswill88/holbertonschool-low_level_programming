bd90746e6e9c3c16b0df8e399a8d5789f69739a8
chriswill88 <williechri79@gmail.com>
2019-07-16 20:54:36 -0400

    task 2 fix for size

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,14 +12,15 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = "\0";
 
 	while (s1[i] != '\0')
 		i++;
 	while (s2[n] != '\0')
 		n++;
+	n++;
 	space = n + i;
 
 	ptr = malloc(space);
@@ -33,5 +34,6 @@ char *str_concat(char *s1, char *s2)
 			ptr[count] = *s2++;
 		count++;
 	}
+
 	return (ptr);
 }
