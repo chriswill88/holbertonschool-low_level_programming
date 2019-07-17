240f2f48dc38b0ac5f3c568d37b898592ebf71d7
chriswill88 <williechri79@gmail.com>
2019-07-16 22:32:26 -0400

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* alloc_grid - retruns a pointer to a 2d grid
+* @height: the height of the array
+* @width: the width of the array
+* Return: a 2d array
+*/
+int **alloc_grid(int width, int height)
+{
+	int i = 0, n = 0;
+	int **w = malloc(height * sizeof(int *));
+
+	if (w == NULL)
+		return (NULL);
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	while (i < height)
+	{
+		w[i] = malloc(width * sizeof(int));
+		if (w[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(w[i]);
+				i--;
+			}
+			return (NULL);
+		}
+		while (n < width)
+		{
+			w[i][n] = 0;
+			n++;
+		}
+		i++;
+	}
+	return (w);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -2,7 +2,9 @@
 #define __HOLBERTON__
 
 #include <stdio.h>
+#include <stdlib.h>
 
+int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
