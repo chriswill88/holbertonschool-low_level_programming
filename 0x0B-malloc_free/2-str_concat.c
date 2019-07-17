477ef30699e4cae5ed802a7eb465b3cd50bbb2b8
chriswill88 <williechri79@gmail.com>
2019-07-16 20:31:13 -0400

    task 2 fin

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,36 +3,25 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, n = 0, space = 0, s1c = 0, count = 0;
-	char *ptr, *start;
-
-	if (s1 == NULL)
-		i++;
-	if (s2 == NULL)
-		*s2 = '\0';
+	int i = 0, n = 0, space = 0, count = 0;
+	char *ptr;
 
 	while (s1[i] != '\0')
 		i++;
 	while (s2[n] != '\0')
 		n++;
-
 	space = n + i;
+	
 	ptr = malloc(space);
 	if (ptr == NULL)
 		return (NULL);
-	start = ptr;
 	while (count < space)
 	{
-		if (i > s1c)
-		{
-			*ptr++ = s1[s1c];
-			s1c++;
-		}
-		else if (*s2 != '\0')
-			*ptr++ = *s2++;
+		if (*s1 != '\0')
+			ptr[count] = *s1++;
+		else
+			ptr[count] = *s2++;
 		count++;
 	}
-	*ptr = *s2;
-	printf("%d", count);
-	return (start);
+	return (ptr);
 }
