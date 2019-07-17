21d6904f314cd5b4210daf0e18f6240b323d7cb2
chriswill88 <williechri79@gmail.com>
2019-07-16 22:41:58 -0400

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	while (i < height)
+	while (i <= height)
 	{
 		w[i] = malloc(width * sizeof(int));
 		if (w[i] == NULL)
