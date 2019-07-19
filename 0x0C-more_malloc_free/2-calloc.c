d5fcec7338060360ebd5bafbe06c92e5ab437ed3
chriswill88 <williechri79@gmail.com>
2019-07-19 15:31:45 -0400

    2file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* _calloc - allocates memory for an array
+* @nemb: number of elements
+* @size: size of type
+* Return: pointer to memory allocation
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *alloc;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	alloc = malloc(nmemb * size);
+	if (alloc == NULL)
+		return (NULL);
+
+	while (i < (nmemb * size))
+	{
+		alloc[i] = 0;
+		i++;
+	}
+	return (alloc);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <limits.h>
 
+
+
+void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 int _atoi(char *s);
