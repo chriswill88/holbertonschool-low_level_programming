6c7ad1a7a49882aea8eeaec8ae3fb535fc90dcc1
chriswill88 <williechri79@gmail.com>
2019-07-18 23:23:50 -0400

    1file fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *cc;
 	int i = 0, size;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i])
 		i++;
-	size = i + n;
+
+	size = i + n + 1;
+	
 	cc = malloc(size);
+	if (cc == NULL)
+		return (NULL);
+	
 	for (i = 0; i < size; i++)
 	{
 		if (*s1)
 			cc[i] = *s1++;
-		else
+		else if (*s2)
 			cc[i] = *s2++;
+		else
+			cc[i] = '\0';
 	}
-	return (cc);
+	
+	cc[size] = '\0';
 
+	return (cc);
 }
