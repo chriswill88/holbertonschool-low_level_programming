266573ec9baaad7a4deb0878e5a9768045e6830a
chriswill88 <williechri79@gmail.com>
2019-07-21 09:49:18 -0400

    task 3 fin: added error handling

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,10 +7,14 @@
 * @argv: the arrguments
 * Return: 0
 */
-int main(int argc __attribute__((unused)), char **argv)
+int main(int argc, char **argv)
 {
 	char *a, *b;
-
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	printf("%ld\n", (strtol(argv[1], &a, 10) * strtol(argv[2], &b, 10)));
 	return (0);
 }
