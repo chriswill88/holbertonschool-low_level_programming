0d8e552a3c6606e64158fe308964ad959e525e84
chriswill88 <williechri79@gmail.com>
2019-07-24 19:42:49 -0400

    checks pointer to function NULL

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,7 +7,7 @@
 */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
-		name = "";
+	if (f == NULL)
+		return;
 	f(name);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,6 +10,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
+	if (cmp == NULL)
+		return;
+
 	if (size <= 0)
 		return (-1);
 	while (size > i)
