beaaa19f40dfaedd148da818f2b8912cfca9402c
chriswill88 <williechri79@gmail.com>
2019-07-24 22:32:33 -0400

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+* main - multiply, divid, add, subtract
+* @argc: number of arguments
+* @argv: arguments
+* Returns: solution
+*/
+void main(int argc, char **argv)
+{
+	int x, y, results = 0;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+
+	if (y == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	results = (get_op_func(argv[2]))(x, y);
+	printf("%d\n", results);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+* op_add - adds two numbers
+* @a: int
+* @b: int
+* Return: sum
+*/
+int op_add(int a, int b)
+{
+	return ((a + b));
+}
+
+/**
+* op_sub - subtracts two numbers
+* @a: int
+* @b: int
+* Return: diffrence
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - nultiplies two numbers
+* @a: int
+* @b: int
+* Return: product
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - divides two numbers
+* @a: int
+* @b: int
+* Return: the divide
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - apllies modulus to a and b
+* @a: int
+* @b: int
+* Return: remainder
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
