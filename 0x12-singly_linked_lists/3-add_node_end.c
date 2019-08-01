6b1604be8e989b27aa6ac8e76ca2c84248417c8d
chriswill88 <williechri79@gmail.com>
2019-08-01 16:37:17 -0400

    checks for null added for 2 and 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,13 +3,16 @@
 * add_node_end - adds node to the end of a list
 * @head: the first node in the list
 * @str: the string to add in the list
-* Return: the front of hte linked list
+* Return: the front of the linked list
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	int i = 0;
-	list_t *new, *ptr = *head;
+	list_t *new, *ptr;
 
+	if (head == NULL || str == NULL)
+		return (NULL);
+	*ptr = head;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
