3bdab1bb7f801dbefc611e9635131904bb13275f
chriswill88 <williechri79@gmail.com>
2019-08-06 22:47:26 -0400

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	while (*head)
+	while (head && *head)
 	{
 		ptr = *head;
 		*head = (*head)->next;
 		free(ptr);
 	}
-	head = NULL;
 }
