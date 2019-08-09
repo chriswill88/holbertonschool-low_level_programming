8842dcbf7e800a0006c24dcb50e0d30acb7a8588
chriswill88 <williechri79@gmail.com>
2019-08-09 00:08:33 -0400

    task 1 done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,28 @@
 #include "holberton.h"
 /**
- * 
+ * print_binary - prints numbers into binary
+ * @n: the number to convert
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1, str = 0;
+	unsigned long int i = 1, num = 0;
 
-
+	if (n == 0)
+		_putchar('0');
 	while (i < n)
 		i <<= 1;
 	while (i)
 	{
+
 		if (n & i)
 			_putchar('1');
 		else
 		{
-			if (str == 0)
-			{
-				str++;
-				continue;
-			}
-			_putchar('0');
+			if (num == 0)
+				num++;
+			else
+				_putchar('0');
 		}
 		i >>= 1;
 	}
