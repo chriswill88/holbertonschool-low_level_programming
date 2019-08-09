84c3836d063657fc45d6621be886e4c763b45cf2
chriswill88 <williechri79@gmail.com>
2019-08-09 19:25:41 -0400

    task 7

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,13 +3,14 @@
 /**
  * clear_bit - turns a bit off
  * @n: the number we are altering
- * @index: the bit 
+ * @index: the bit
  * Return: 1 if success, -1 if not success
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i = 1;
-	if (i > 63)
+
+	if (index > 63)
 		return (-1);
 	i <<= index;
 	*n &= ~i;
