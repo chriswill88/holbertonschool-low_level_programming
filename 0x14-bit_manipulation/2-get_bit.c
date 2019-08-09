6f8a814650f749f2a5e3616494dff368c41245fe
chriswill88 <williechri79@gmail.com>
2019-08-09 14:23:07 -0400

    fix for -1 return

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,6 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int i = 1;
+
+	if ((i <<= index) > n)
+		return (-1);
 	if (((n >> index) & 1) == 0)
 		return (0);
 	else if (((n >> index) & 1) == 1)
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -10,7 +10,7 @@ int main(void)
 {
     int n;
 
-    n = get_bit(2, 10);
+    n = get_bit(2, 1);
     printf("%d\n", n);
     n = get_bit(98, 1);
     printf("%d\n", n);
