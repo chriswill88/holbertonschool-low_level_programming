ae5860922e88334a9f4856dcf834f01b29c9211a
chriswill88 <williechri79@gmail.com>
2019-08-09 19:23:07 -0400

    task 7

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - turns a bit off
+ * @n: the number we are altering
+ * @index: the bit 
+ * Return: 1 if success, -1 if not success
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1;
+	if (i > 63)
+		return (-1);
+	i <<= index;
+	*n &= ~i;
+	return (1);
+}
