eca4594c924243ada293330e31070f6c3596bbdd
chriswill88 <williechri79@gmail.com>
2019-08-09 09:55:26 -0400

    printing 0 in task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,11 +10,13 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 		_putchar('0');
+
 	while (i < n && o < 63)
 	{
 		i <<= 1;
 		o++;
 	}
+
 	while (i)
 	{
 		if (n & i)
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
-
+/**
+ * 
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	if (((n >> index) & 1) == 0)
