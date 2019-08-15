a60072083a3edb1f1796eb71479442586bb71497
chriswill88 <williechri79@gmail.com>
2019-08-15 00:36:01 -0400

    checks added

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,14 +47,15 @@ int main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		errnots(1, argv, fd);
-	df = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	df = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0777);
 	if (df == -1)
-		errnots(0, argv, fd);
+		errnots(2, argv, fd);
 	while (1)
 	{
 		rd = read(fd, buf, 1024);
 		if (rd == -1)
 			errnots(1, argv, fd);
+	
 		if (buf[0])
 		{
 			wr = write(df, buf, rd);
