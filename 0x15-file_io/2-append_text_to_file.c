c061808fc4022a889fc580d908e62292077b83e5
chriswill88 <williechri79@gmail.com>
2019-08-14 20:16:53 -0400

    the cp function operable

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,8 +14,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
+	{
+		printf("failed at open");
 		return (-1);
-
+	}
 	if (text_content == NULL)
 	{
 		close(fd);
@@ -26,9 +28,12 @@ int append_text_to_file(const char *filename, char *text_content)
 		i++;
 
 	wr = write(fd, text_content, i);
+	
 	if (wr == -1)
+	{
+		printf("failed at write");
 		return (-1);
-
+	}
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,29 +1,72 @@
 #include "holberton.h"
 
-int main(int argc, char **argv)
+/**
+ * errnots - the error opertations
+ * @i: which error
+ * @argv: the arguments for shell
+ * @fd: the file decriptor
+ * Return: void
+ */
+void errnots(int i, char **argv, int fd)
 {
-	int fd;
-	int df;
-	
-	if (argc != 3)
+	if (i == 0)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	else if (i == 1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n")
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	
-
-	df = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (df == -1)
+	else if (i == 2)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n")
-
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	else if (i == 3)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
 }
+/**
+ * main - cp function
+ * @argc: the number of arguments
+ * @argv: the arguments
+ * Return: 0 if success
+ */
 
+int main(int argc, char **argv)
+{
+	int fd = 0, df, rd, wr;
+	char buf[1024];
+
+	if (argc != 3)
+		errnots(0, argv, fd);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		errnots(1, argv, fd);
+	df = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (df == -1)
+		errnots(0, argv, fd);
+	while (1)
+	{
+		rd = read(fd, buf, 1024);
+		if (rd == -1)
+			errnots(1, argv, fd);
+		if (buf[0])
+		{
+			wr = write(df, buf, rd);
+			if (wr == -1)
+				errnots(2, argv, fd);
+			if (wr < 1024)
+				break;
+		}
+	}
+	if (close(df) == -1)
+		errnots(3, argv, df);
+	if (close(fd) == -1)
+		errnots(3, argv, fd);
+	return (0);
+}
