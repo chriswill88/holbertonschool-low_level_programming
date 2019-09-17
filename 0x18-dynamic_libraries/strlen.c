480b04a80b0b0bd31ba75f64d6de2eb8256736ae
chriswill88 <williechri79@gmail.com>
2019-09-16 23:41:01 -0400

    strlen betty compliant

--- 0x18-dynamic_libraries/strlen.c
+++ 0x18-dynamic_libraries/strlen.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
+/**
+ * _strlen - gets the length of strings
+ * @s: pinter to a string
+ * Return: the string length
+ */
 int _strlen(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while(s[i++])
-        ;
+	while (s[i++])
+		;
 
-    return (i - 1);
+	return (i - 1);
 }
