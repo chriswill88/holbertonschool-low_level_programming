d721d7c27887d0a26fa4b2ca310bc963e64095d5
Chris Williams <46333279+chriswill88@users.noreply.github.com>
2019-09-18 09:51:42 -0400

    task 2 fix: fail check for null, head prev to new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,16 +9,22 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
-	if (*head && *head != NULL)
-		while ((*head)->prev != NULL)
-			*head = (*head)->prev;
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
+	
+	if (*head != NULL)
+	{
+		while ((*head)->prev != NULL)
+			*head = (*head)->prev;
+		(*head)->prev = new;
+	}
 	*head = new;
 
 	return (new);
