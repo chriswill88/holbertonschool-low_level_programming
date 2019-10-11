7bb534151e59542d2fa5b81767c50f2a1607bf15
chriswill88 <williechri79@gmail.com>
2019-10-11 14:43:24 -0400

    task 0 sizeof hash node ptr type

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,17 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int x = 0;
 	hash_table_t *newhash;
 
 	newhash = malloc(sizeof(hash_table_t));
 	if (newhash == NULL)
 		return (NULL);
 	newhash->size = size;
-	newhash->array = calloc(size, sizeof(hash_node_t));
+	newhash->array = malloc(sizeof(hash_node_t *) * newhash->size);
 	if (newhash->array == NULL)
 		return (NULL);
+	while (x < size)
+		newhash->array[x++] = NULL;
 	return (newhash);
 }
