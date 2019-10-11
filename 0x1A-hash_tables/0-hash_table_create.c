426922227353696755d4636602efec82a4af70d8
chriswill88 <williechri79@gmail.com>
2019-10-11 13:49:45 -0400

    task 0 documentation and betty compliant

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,15 +1,19 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the hash table
+ * Return: a hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *newhash;
-    
-    newhash = malloc(sizeof(hash_table_t));
-    if (newhash == NULL)
-        return (NULL);
-    newhash->size = size;
-    newhash->array = malloc(sizeof(hash_node_t) * newhash->size);
-    if (newhash->array == NULL)
-        return (NULL);
-    return (newhash);
+	hash_table_t *newhash;
+
+	newhash = malloc(sizeof(hash_table_t));
+	if (newhash == NULL)
+		return (NULL);
+	newhash->size = size;
+	newhash->array = malloc(sizeof(hash_node_t) * newhash->size);
+	if (newhash->array == NULL)
+		return (NULL);
+	return (newhash);
 }
