38831f91e74d0d0dd587e33529f4f51ce14418a3
chriswill88 <williechri79@gmail.com>
2019-10-13 01:08:41 -0400

    task 3 fix: what if theres no hash table Ret 0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx = key_index((const unsigned char *)key, ht->size);
 	hash_node_t **ary = ht->array, *new, *ptr;
 
-	if (key == NULL || !strlen(key))
+	if (key == NULL || strlen(key) == 0 || !ht)
 		return (0);
 	if (!ary[idx])
 	{
