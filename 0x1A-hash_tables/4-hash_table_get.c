a5a80514f18d5a7c720c06820a98222d4e8133df
chriswill88 <williechri79@gmail.com>
2019-10-13 05:27:34 -0400

    updated hash_tables.h: fixed collisions

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,11 +7,25 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int idx;
+	hash_node_t **ptr, *node;
+
 	if (!ht || ht->array == NULL || !key)
 		return (NULL);
-	if (!ht->array[key_index((const unsigned char *)key, ht->size)])
+
+	ptr = ht->array;
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (!ptr[idx])
 		return (NULL);
-	if (!ht->array[key_index((const unsigned char *)key, ht->size)]->value)
+	if (!(ptr[idx])->key)
 		return (NULL);
-	return (ht->array[key_index((const unsigned char *)key, ht->size)]->value);
+	node = ptr[idx];
+	while (node)
+	{
+/*string compare for whether to create a new node or just update a key*/
+		if (!strcmp(key, node->key))
+			return (node->value);
+		node = node->next;
+	}
+	return (ht->array[idx]->value);
 }
