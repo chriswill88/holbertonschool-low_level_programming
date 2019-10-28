377e80b4f8434d1ced950be2dc6afc4230e2c409
chriswill88 <williechri79@gmail.com>
2019-10-28 06:53:28 -0400

    [fix] placement of the print array

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -19,11 +19,11 @@ void bubble_sort(int *array, size_t size)
 				array[ind] = array[ind + 1];
 				array[ind + 1] =  hold;
 				n = 1;
+				print_array(array, size);
 				break;
 			}
 			else
 				n = 0;
 		}
-		print_array(array, size);
 	}
 }
