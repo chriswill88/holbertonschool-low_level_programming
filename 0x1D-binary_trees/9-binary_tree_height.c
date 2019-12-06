f9af2f7560bb2427489b9307489dbd23305eb44a
chriswill88 <williechri79@gmail.com>
2019-12-06 09:40:58 -0500

    [fix] task 9

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -1,27 +1,21 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - get the height of a node
+ * binary_tree_height - get the height of a tree
  * @tree: ptr to tree
  * Return: height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-		return (_lambda4(tree, 0));
-	return (0);
-}
+	size_t n = 0;
 
-/**
- * _lambda4 - gets height
- * @tree: returns size
- * @n: size to return
- * Return: height
- */
-size_t _lambda4(const binary_tree_t *node, size_t n)
-{
-	if (node->left)
-		_lambda4(node->left, n += 1);
-	if (node->right)
-		_lambda4(node->right, n += 1);
-	return (n);
+	if (tree)
+	{
+		if (tree->left)
+			n += 1;
+		if (tree->right)
+			n += 1;
+
+		return (n += binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
 }
