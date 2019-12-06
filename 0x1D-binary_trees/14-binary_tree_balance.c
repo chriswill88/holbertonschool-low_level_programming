a5d0b9ec745bf6a3af5683cf759ae66d1d255651
chriswill88 <williechri79@gmail.com>
2019-12-06 12:12:30 -0500

    [Fix] task 14 - betty compliant

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -6,20 +6,38 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int n = 0;
-	const binary_tree_t *l, *r;
+	int left = 0, right = 0;
 
 	if (tree)
 	{
 		if (tree->left)
-			n += 1;
+			left = _lambda4(tree->left, 0);
 		if (tree->right)
-			n -= 1;
-		l = tree->left;
-		r = tree->right;
-
-		return (binary_tree_balance(l) - binary_tree_balance(r) + n);
+			right = _lambda4(tree->right, 0);
+		/*printf("[%d] l = %d r = %d\n", tree->n, left, right);*/
+		return (left - right);
 	}
 	return (0);
 }
 
+
+/**
+ * _lambda4 - gets height
+ * @node: returns size
+ * @n: size to return
+ * Return: height
+ */
+size_t _lambda4(const binary_tree_t *node, size_t n)
+{
+	size_t l = 0, r = 0;
+
+	if (!node)
+		return (0);
+	if (node->left)
+		l = _lambda4(node->left, n);
+	if (node->right)
+		r = _lambda4(node->right, n);
+	if (l > r)
+		return (l + 1);
+	return (r + 1);
+}
