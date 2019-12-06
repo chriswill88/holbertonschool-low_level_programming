00d301833649f154710884047d264d2496e199f0
chriswill88 <williechri79@gmail.com>
2019-12-06 14:22:43 -0500

    [Fix] task 15

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -6,19 +6,19 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	binary_tree_t *left, *right;
+	binary_tree_t *left = NULL, *right = NULL;
 
 	if (!tree)
 		return (1);
 
 	if (tree)
 	{
-		if (tree->left && tree->right)
-		{
+		if (tree->left)
 			left = tree->left;
+		if (tree->right)
 			right = tree->right;
+		if (left && right)
 			return (binary_tree_is_full(left) && binary_tree_is_full(right));
-		}
 		if (!(tree->left) && !(tree->right))
 			return (1);
 	}
