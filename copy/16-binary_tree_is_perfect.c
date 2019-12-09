1393ca27a34f00254f8e3e3a986054fc95350ec1
chriswill88 <williechri79@gmail.com>
2019-12-09 07:21:04 -0500

    [Fix] task 16

---
+++ copy/16-binary_tree_is_perfect.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_perfect - checks if binary tree is perfect
+ * @tree: ptr to tree
+ * Return: 0 or 1
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	const binary_tree_t *ptr;
+	int i = 0;
+
+	if (!tree)
+		return (1);
+	ptr = tree;
+	if (ptr->left)
+		while (ptr->left)
+		{
+			ptr = ptr->left;
+			i++;
+		}
+
+	/*printf("height = %d\n", i);*/
+	if ((perfect_return(tree, i)) == i)
+		return (1);
+	return (0);
+}
+
+/**
+ * perfect_return - returns if tree is perfect or not
+ * @tree: the binary tree
+ * Return: value corresponding to if tree is perfect
+ */
+int perfect_return(const binary_tree_t *tree, int height)
+{
+	int l = 0, r = 0;
+
+	/*printf("node - n = %d\n", tree->n);*/
+
+	if (tree->right && tree->left)
+	{
+		l += perfect_return(tree->left, height);
+		r += perfect_return(tree->right, height);
+	}
+	if (!tree->right && !tree->left)
+	{
+		l -= 1;
+		r -= 1;
+	}
+
+	/*printf("l = %d, r = %d, height %d\n", l, r, height);*/
+	if (l > r)
+		return (l + 1);
+	return (r + 1);
+}
