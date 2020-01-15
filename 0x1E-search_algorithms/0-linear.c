edc1f052229c3bf0543d1a27dab5f763976f4854
chriswill88 <williechri79@gmail.com>
2020-01-15 14:47:33 -0500

    [FIX] task 0 betty compliant

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,14 +8,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    int *ptr = array;
-    size_t i = 0;
+	int *ptr = array;
+	size_t i = 0;
 
-    for (i = 0; array && i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (ptr[i] == value)
-            return (i);
-    }
-    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (ptr[i] == value)
+			return (i);
+	}
+	return (-1);
 }
