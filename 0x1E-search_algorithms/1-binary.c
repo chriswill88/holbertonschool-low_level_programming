3eac24466f3adca2036d555b51bf810a2241ac94
chriswill88 <williechri79@gmail.com>
2020-01-16 21:35:52 -0500

    [ADD + UPDATE] task 2 and update for header file

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+#include <unistd.h>
+#include <math.h>
+/*
+ * binary_search - using binary search to find value in list
+ * @array: the array of ints
+ * @size: the size of the array
+ * @value: the value to find in array
+ * Return: the index where the value was found or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l = 0, r = size - 1, half = 0, i;
+
+	while (l <= r)
+	{
+		half = (l + r) / 2;
+		printf("Searching in array: ");
+		for (i = l; i <= r ; i++)
+		{
+			printf("%d", array[i]);
+			if (i < r)
+				printf(", ");
+			else
+				printf("\n");
+		}
+
+		if (array[half] == value)
+			return (half);
+		else if (array[half] < value)
+			l = half + 1;
+		else
+			r = half - 1;
+	}
+	return (-1);
+}
