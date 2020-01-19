5f195b80006e47e7303ac6a611256f9ce42d64a9
chriswill88 <williechri79@gmail.com>
2020-01-19 05:25:45 -0500

    [FIX] task 100: output fix, fixed numbers before output

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -9,19 +9,25 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t tsize = sqrt(size), inx = 0, ind = 0;
+	size_t tsize = sqrt(size), inx = 0, ind = 0, btw = 0;
 
 	if (!array || !size)
 		return (-1);
+	/*printf("the sqrt = %ld\n--------------------\n", tsize);*/
 	for (inx = 0; inx < size; inx += tsize)
 	{
-		printf("1 Value checked array[%ld] = [%d]\n", inx, array[inx]);
-		if ((inx + tsize) < size && value <= array[inx + tsize])
+		printf("Value checked array[%ld] = [%d]\n", inx, array[inx]);
+		if (inx + tsize >= size)
+			btw = size - 1;
+		else
+			btw = tsize + inx;
+
+		if (value <= array[btw])
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n", inx, inx + tsize);
-			for (ind = inx; ind <= (inx + tsize); ind++)
+			for (ind = inx; ind <= inx + tsize && ind < size; ind++)
 			{
-				printf("2 Value checked array[%ld] = [%d]\n", ind, array[ind]);
+				printf("Value checked array[%ld] = [%d]\n", ind, array[ind]);
 				if (array[ind] == value)
 					return (ind);
 			}
